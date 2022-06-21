d0df0a291d25eb2742c573710f23affd760be33a
Natalya32 <tigra-pochta@mail.ru>
2022-06-21 23:53:55 +0300

    Commit init

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 5000;
+        int priceMile = 20;
+        int bonus = priceTicket / priceMile;
+
+        System.out.println(bonus);
+    }
+}
